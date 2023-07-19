be9212affdcd12c14081bf394b30fa995be6bed8
Diego Lojan <diego.lojan@unl.edu.ec>
2023-07-18 22:12:44 -0500

    Commit tradu

--- SistemaTutorias/forms.py
+++ SistemaTutorias/forms.py
@@ -8,7 +8,7 @@ class DateInput(forms.DateInput):
 
 
 class AddStudentForm(forms.Form):
-    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    email = forms.EmailField(label="Correo", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
     password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control"}))
     first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
