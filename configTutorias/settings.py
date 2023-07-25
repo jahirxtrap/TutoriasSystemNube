97121f1fbef90dcaf568df2a40214297820a1077
jahirxtrap <jahirroman2002@gmail.com>
2023-07-25 17:56:49 -0500

    clean y arreglo statics

--- configTutorias/settings.py
+++ configTutorias/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/TemaAzul/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/TemaAzul'),
 ]
 
 MEDIA_URL = '/media/'
